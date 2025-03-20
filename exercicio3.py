aed2c3e02a166b7dffdfda770d22dc5a609772ca
Gabriel Augusto da Silva Xavier <gasxavier17@gmail.com>
2025-03-20 14:18:53 -0400

    Add files via upload
    Ex estrutura de dados

---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+num1=int(input("Insira o primeiro número"))
+num2=int(input("Insira o segundo número"))
+
+soma=num1+num2
+sub=num1-num2
+mult=num1*num2
+print(f"A soma deles é: {soma}, a subtração é: {sub}, a multiplicação é: {mult}")
---
+++ index.py
@@ -0,0 +1,15 @@
+#Isto é um comentário
+print("")
+# idade=6
+# altura=0.50
+# peso=5.950
+# nome="Francisco"
+# print(nome)
+# nome=nome+" é uma boa pessoa"
+# print(nome)
+nome=input("insira seu nome: ")
+idade=int(input("Insira sua idade: "))#int=inteiro, str=sting, float= 
+peso=float(input("Insira seu peso: "))
+
+print(f"{nome}, {idade} anos, {peso}kg")
+
